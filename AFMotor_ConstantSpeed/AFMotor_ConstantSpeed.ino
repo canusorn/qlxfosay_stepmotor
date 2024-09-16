85f11ab189ab060f9e07080c3c387bef212c585f
canusorn <39732595+canusorn@users.noreply.github.com>
2024-09-16 17:33:38 +0700

    start test new file

---
+++ AFMotor_ConstantSpeed/AFMotor_ConstantSpeed.ino
@@ -0,0 +1,50 @@
+//Red: A+
+//Green: A-
+//Yellow: B+
+//Blue: B-
+
+
+#include <AccelStepper.h>
+
+// Define the pins for stepper motor control
+#define DIR_PIN_ 8
+#define DIR_PIN 9
+#define STEP_PIN_ 10
+#define STEP_PIN 11
+#define ENABLE_PIN_ 12
+#define ENABLE_PIN 13
+
+// Create an instance of AccelStepper
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
+void setup() {
+  pinMode(DIR_PIN_, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN_, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(ENABLE_PIN_, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+  
+  digitalWrite(DIR_PIN_, LOW); 
+  digitalWrite(DIR_PIN, LOW); 
+  digitalWrite(STEP_PIN_, LOW); 
+  digitalWrite(STEP_PIN, LOW); 
+  digitalWrite(ENABLE_PIN_, LOW); 
+  digitalWrite(ENABLE_PIN, LOW); 
+  
+  stepper.setMaxSpeed(5000);     // Set maximum speed (steps per second)
+  stepper.setAcceleration(500);  // Set acceleration (steps per second^2)
+}
+
+void loop() {
+  // Set the target position (e.g., 1000 steps)
+  stepper.moveTo(10000);
+
+  // Run the motor to the target position
+  stepper.run();
+
+  // If the motor reaches the target position, move it back to the starting point
+  if (stepper.distanceToGo() == 0) {
+    stepper.moveTo(-stepper.currentPosition());
+  }
+}
